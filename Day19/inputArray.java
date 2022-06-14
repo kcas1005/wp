7c369dfd33ef08deb92e9b586d79dd4d46ebfaef
kcas1005 <kcas1005@naver.com>
2022-06-14 17:02:09 +0900

    20일차

--- Day19/inputArray.java
+++
@@ -1,5 +0,0 @@
-package hello.main;
-
-public class inputArray {
-
-}
---
+++ Day20/Day19_두개뽑아서더하기_봉해답.java
@@ -0,0 +1,21 @@
+import java.util.*;
+    
+class Solution {
+    public static main(String[] args){
+        
+        System.out.println(solution());
+    }
+    
+    public TreeSet<Integer> solution(int[] numbers) {
+    TreeSet<Integer> answer = new TreeSet<Integer>();
+                
+        for (int i = 0; i<numbers.length; i++){ 
+            for (int j = i+1; j<numbers.length; j++) {
+                answer.add(numbers[i] + numbers[j]);
+                
+            } System.out.println(answer);  
+        }
+    
+        return answer;
+    }
+}
