742f2058cf294caf3cc04eaa05692f949ba09e8d
kcas1005 <kcas1005@naver.com>
2022-06-14 23:52:25 +0900

    집에서 함

--- Day20/Day19_두개뽑아서더하기ret.java
+++ Day20/Day19_두개뽑아서더하기ret.java
@@ -1,14 +1,28 @@
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.util.*;
 
-public class Day19_두개뽑아서더하기 {
+class Day19_두개뽑아서더하기ret {
     public static void main(String[] args) {
-      
-        
-        System.out.println(b);
+        int[] numbers = { 5, 0, 2, 7 };
+        a.solution(numbers);
     }
+
 }
 
-// 순서 오름차순 int형으로
-// a[x] = a[x]가 나오면 안됨
-// a[x] == a[x+1]+a[y]
+class a {
+    public static TreeSet<Integer> solution(int[] numbers) {
+        TreeSet<Integer> answer = new TreeSet<Integer>();
+        int length = numbers.length;
+        // System.out.println("numbers.length: " + length);
+        // System.out.println(Arrays.toString(numbers));
+        for (int y = 0; y < length; y++) {
+            for (int z = y; z < length; z++) {
+                if (y!=z) {
+                    answer.add(numbers[y] + numbers[z]);
+                }
+            }
+
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
