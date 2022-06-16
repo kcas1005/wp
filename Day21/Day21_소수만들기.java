29cf07c363917298e77e60f5e2550f47e7771f37
kcas1005 <kcas1005@naver.com>
2022-06-16 21:37:01 +0900

    집에서

--- Day21/Day21_소수만들기.java
+++ Day21/Day21_소수만들기.java
@@ -1,31 +1,70 @@
+package Day21;
+
+import java.util.*;
+
 public class Day21_소수만들기 {
     public static void main(String[] args) {
-        int[] nums = { 1, 2, 3, 4 };
-        C.solution(nums);
+        int[] numbers = { 1, 2, 3, 4 };
+        SolutionE.solution(numbers);
     }
 }
 
-class C {
-    public static int solution(int[] nums) {
-        int answer = 0;
+class SolutionE {
+    public static TreeSet<Integer> solution(int[] numbers) {
+        TreeSet<Integer> tree = new TreeSet<>();
         int sum = 0;
         int count = 0;
-        for (int a = 0; a < nums.length; a++) {
-            for (int b = a + 1; a < nums.length; b++) {
-                for (int c = b + 1; c < nums.length; c++) {
-                    for (int d = 1; d < nums[a]+nums[b]+nums[c]; d++) {
-                        if ((a != b) && (a != c) && (b != c)) {
-                            if (nums[a] + nums[b] + nums[c] % d == 0) {
+        int answer = 0;
+        int length = numbers.length;
 
-                                sum = nums[a] + nums[b] + nums[c];
-                                System.out.println("sum: " + sum);
-                                count++;
-                            }
-                        }
+        for (int a = 0; a < length; a++) {
+            for (int b = a+1; a < length; b++) {
+                for (int c = b+1; c < length; c++) {
+                    sum = numbers[a] + numbers[b] + numbers[c];
+                    tree.add(sum);
+                    if(SolutionF.Is_Prime(sum)){
+
                     }
                 }
             }
         }
-        return count;
+        return tree;
+    }
+}
+
+class SolutionF {
+    public class small {
+
+        public static int solutionH() {
+            int[] num = { 1, 2, 3, 4, 5 };
+            int count = 0;
+            int result = Is_Prime(num);
+
+            if (result == 2) {
+                System.out.println(num + "은 소수입니다.");
+            } else if (result < 2) {
+                System.out.println("1보다 큰 양의 정수를 입력해주세요.");
+            } else {
+                System.out.println(num + "은 소수가 아닙니다.");
+            }
+            return count;
+        }
+
+        public static int Is_Prime(int[] num) {
+            int count = 0;
+
+            LinkedList<Integer> answer = new LinkedList<Integer>();
+
+            for (int i = 1; i <= num.length; i++) {
+                for (int a = 1; a <= num[i]; a++) {
+                    if (num[i] % a == 0) {
+                        count += 1;
+                    }
+                    if (count >= 3) {
+                    }
+                }
+            }
+            return count;
+        }
     }
 }
--- small.java
+++ small.java
@@ -1,9 +1,12 @@
+import java.util.LinkedList;
+
 public class small {
 
     public static void main(String[] args) {
-        int[] num = { 1, 2, 3, 4 };
+        int[] num = {1,2,3,4,5};
         int count = 0;
         int result = Is_Prime(num);
+
         if (result == 2)
             System.out.println(num + "은 소수입니다.");
         else if (result < 2)
@@ -12,16 +15,20 @@ public class small {
             System.out.println(num + "은 소수가 아닙니다.");
     }
 
-    private static int Is_Prime(int num) {
+    private static int Is_Prime(int[] num) {
         int count = 0;
 
-        for (int i = 1; i <= num; i++) {
-            if (num % i == 0)
-                count += 1;
-            if (count >= 3)
-                return count;
-        }
+        LinkedList<Integer> answer = new LinkedList<Integer>();
 
+        for (int i = 1; i <= num.length; i++) {
+            for(int a = 1; a <= num[i]; a++){
+            if (num[i] % a == 0){
+                count += 1;
+            }
+            if (count >= 3){
+            }
+            }
+        }
         return count;
     }
 }
