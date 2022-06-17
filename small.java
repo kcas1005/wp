8937eb2231827bff19ae2238e9ef6beaa8406f65
kcas1005 <kcas1005@naver.com>
2022-06-17 16:14:16 +0900

    오늘 한것

--- small.java
+++ small.java
@@ -1,30 +1,28 @@
 import java.util.ArrayList;
 
 class small {
-    static void main(String[] args) {
-		int[][] score = { { 79, 80, 99 }, // A학생
-						  { 95, 85, 89 }, // B학생
-						  { 90, 65, 56 }, // C학생
-						  { 69, 78, 77 } }; // D학생
-					// 과목 : a   b   c
-		int[] student = new int[4]; // 학생 총점 배열 생성
-		int[] subject = new int[3]; // 과목 총점 배열 생성
-		String[] stuName = {"A학생", "B학생", "C학생", "D학생"}; // 학생명 배열 저장
-		String[] subName = {"영어", "수학", "과학"}; // 과목명 배열 저장
+	public static void main(String[] args) {
+		int[] num = {1,2,3,4};
 
-		for (int i = 0; i < student.length; i++) {
-			for (int j = 0; j < subject.length; j++) {
-				student[i] += score[i][j]; // 학생별 총점 계산
+		solutionB(nums);
+
+	public static int solutionA (int[] nums) {
+		int answer = 0;
+
+		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+		System.out.println("Hello Java");
+
+		return answer;
+	}
+	public static int[] solutionB (int[] nums)
+
+	for(int a=0; a<nums.length; a++){
+		for(int b=0; b<nums.length; b++){
+			for(int c=0; c<nums.length; c++){
+
 			}
-			System.out.println(stuName[i] + " 총점 : " + student[i]);
-		}
-		System.out.println("-----------------");
-		for (int j = 0; j < subject.length; j++) {
-			for (int i = 0; i < student.length; i++) {
-				subject[j] += score[i][j]; // 과목별 총점 계산
-			}
-			System.out.println(subName[j]+" 총점 : " + subject[j]);
 		}
 	}
+	return null;
+	}
 }
-https://m.blog.naver.com/heartflow89/220950845259
