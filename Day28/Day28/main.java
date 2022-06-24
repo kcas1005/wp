b4e08059bbb4f697bbc5ee1d188b0738a02fbb67
kcas1005 <wltjdwls9@naver.com>
2022-06-24 13:40:22 +0900

    시험

---
+++ Day28/Day28/main.java
@@ -0,0 +1,26 @@
+package Day28;
+public class main{
+    public static void main(String[] args){
+
+        usePotion();
+
+    }
+        private static void usePotion() {
+            Redpotion redpotion = new Redpotion(30, 1);
+            Redpotion redpotion_x3 = new Redpotion(30, 3);
+
+            redpotion.usePotionText();
+
+            int effectHP = redpotion.useItem(20);
+            int effectHP_x3 = redpotion_x3.useItem(30);
+
+            System.out.println("포션사용" + effectHP);
+            System.out.println("포션사용" + effectHP_x3);
+
+            
+        }
+
+
+
+    
+}
---
+++ Day28/item.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class item {
+    static int Data;
+    static String[] Item_List = { "고양이의 검", "개의 검", "코끼리의 검", "고릴라의 검" };
+    static String[] Item_Monster = new String[10];
+
+    public static void main(String[] arg) {
+
+        int random = (int) (Math.random() * 101) + 0;
+        if (random > 80) {
+            System.out.println("아이템 획득");
+        } else {
+            System.out.println("아이템 획득 실패");
+            MonsterItem();
+        }
+
+    }
+
+    public static int Itemlist(String[] Item_List) {
+
+        int Item_random = (int) (Math.random() * Item_List.length) + 0;
+        int Data = Item_random;
+        // System.out.println("Data: " + Data);
+        return Data;
+    }
+
+    public static void MonsterItem() {
+        int MIData = Itemlist(Item_List);
+        for (int i = 0; i < Item_Monster.length; i++) {
+
+            if (Item_Monster[i] != null) {
+                continue;
+            } else if (Item_Monster[i] == null) {
+                Item_Monster[i] = Item_List[MIData];
+                // Item_Monster[i]=get(Item_List[MIData]);
+
+                System.out.println(i + 1 + "번째 창에 " + Item_List[MIData] + " 아이템을 집어넣습니다.");
+                System.out.println("아이템 창 확인 : " + Arrays.toString(Item_Monster));
+                break;
+            } else {
+                System.out.println("인벤토리 창 확인 하세요.");
+                System.out.println("아이템 창 확인 : " + Arrays.toString(Item_Monster));
+                break;
+            }
+
+        }
+
+    }
+
+}
