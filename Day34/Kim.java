5529cd31753dd72e3614c17a748ad8d9f0bf2afc
kcas1005 <wltjdwls9@naver.com>
2022-07-05 17:16:01 +0900

    업로드

--- Day34/Kim.java
+++ Day34/Kim.java
@@ -1,6 +1,9 @@
-public class Kim implements Remote{
-    // 오빠 메롱                                                                  ^ㅠ^
-    public void tv_remote(int input_channel){
+public class Kim implements Remote, Login {
+    String id = "Kim";
+    String pw = "Kim123";
+    // 오빠 메롱 ^ㅠ^
+
+    public void tv_remote(int input_channel) {
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
@@ -8,5 +11,23 @@ public class Kim implements Remote{
             e.printStackTrace();
         }
         System.out.println("5초 기다림 끝 " + input_channel + "번 화면 출력");
+    }
+
+    public void favorit_channel() {
+
+    }
+
+    public void Login(String id, String pw) {
+        if (this.id == id) {
+            if(this.pw == pw){
+                System.out.println("로그인 되었습니다.");
+            }
+            else{
+                System.out.println("비밀번호가 다릅니다.");
+            }
+
+        } else {
+            System.out.println("Kim의 아이디가 다릅니다.");
+        }
     }
 }
