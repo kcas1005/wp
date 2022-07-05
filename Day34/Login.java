1058ab3fb5e8341d695ee2d08a7263a3b64bdb40
kcas1005 <wltjdwls9@naver.com>
2022-07-06 00:08:50 +0900

    집에서 조금

--- Day34/Login.java
+++ Day34/Login.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 interface Login {
     public String id = null;
     public String pw = null;
@@ -6,8 +5,3 @@ interface Login {
 
     void Login(String id, String pw, String fc);
 }
-=======
-public interface Login {
-    void Login(String id, String pw);
-}
->>>>>>> 3f2dc3ccafaf536266e7043a5c83af5d2d5ebce4
