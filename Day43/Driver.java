8d858de67b2e1de134a3a509c4ec96e6cdbe578f
kcas1005 <wltjdwls9@naver.com>
2022-07-15 17:28:00 +0900

    학원에서 채널3 db 넣는 중

---
+++ Day43/Driver.java
@@ -0,0 +1,12 @@
+package Day43;
+
+public class Driver {
+    public static void main(String[] args) {
+        String[] driver = {"휴먼","지성진"};
+
+        String not_run = "드라이버는 달릴 수 없습니다.";
+
+        System.out.println("0번째 : " + driver[0]);
+        System.out.println("1번째 : " + driver[1]);
+    }
+}
