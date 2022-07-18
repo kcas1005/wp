1d14ba8ab66ba29ef0095a022eabc32680d95599
kcas1005 <wltjdwls9@naver.com>
2022-07-18 17:13:47 +0900

    학원에서 함

---
+++ Day44/exam01_nestedinterface/CallListener.java
@@ -0,0 +1,8 @@
+package Day44.exam01_nestedinterface;
+
+public class CallListener implements Button.OnClickListener{
+    @Override
+    public void onClick(){
+        System.out.println("전화를 겁니다.");
+    }
+}
