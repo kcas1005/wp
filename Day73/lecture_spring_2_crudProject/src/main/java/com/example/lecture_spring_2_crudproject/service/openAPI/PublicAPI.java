16211644623807baa4c74bfe54eb7c0f842e4be8
kcas1005 <wltjdwls9@naver.com>
2022-08-31 17:24:25 +0900

    Day74_이미지 서버에 저장하는 것 배움 DB에 연동하는 건 내일 배우기로 함

--- Day73/lecture_spring_2_crudProject/src/main/java/com/example/lecture_spring_2_crudproject/service/openAPI/PublicAPI.java
+++ Day73/lecture_spring_2_crudProject/src/main/java/com/example/lecture_spring_2_crudproject/service/openAPI/PublicAPI.java
@@ -20,6 +20,12 @@ public class PublicAPI {
 
         String result = readAPI();
 
+        //jacson : 빅데이터 등 큰 사이즈의 json처리
+        //gson : 분산 아키텍쳐 설정 등 작은 용량의 json
+        //simplejson : 골고루 빠름
+
+        //json방식은 http 프로토콜을 통해서 데이터 전송규약(클라이언트와 백엔드 통신)
+        //백엔드와 백엔드 통신도 json방식 > grpc 통신의 등작으로 다수의 백엔드 통신은 grpc 변경.(앱통신도 grpc)
 
         //Gson tranfer
         Gson pretty = new GsonBuilder().setPrettyPrinting().create();
@@ -27,6 +33,8 @@ public class PublicAPI {
         System.out.println("----------testAPI-----------");
         System.out.println(element);
 
+        //String 문자열을 dto 객체로 변환
+        //fromJson(문자열, DTO객체.class(런타임시점 객체))
         BusDTO busdto = pretty.fromJson(result, BusDTO.class);
         for(int i =0; i<busdto.getResponse().getBody().getNumOfRows(); i++) {
             System.out.println(busdto.getResponse().getBody().getItems().get(i).getCpname());
@@ -56,6 +64,7 @@ public class PublicAPI {
                 sb.append(brResult);
                 System.out.println(brResult);
             }
+            //자원 반환
             br.close();
             con.disconnect();
         }catch(Exception e) {
